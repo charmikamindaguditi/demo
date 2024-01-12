a6abf7acbd6141df6005983a4f7255498823ffa8
charmi <155721667+charmikamindaguditi@users.noreply.github.com>
2024-01-12 10:33:46 +0530

    Update typo.py

--- typo.py
+++ typo.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.header('suzume')
-st.write('suzume')
+st.header('Anime')
+st.write('Suzume')
 st.image('https://www.themoviedb.org/t/p/original/vIeu8WysZrTSFb2uhPViKjX9EcC.jpg',caption='suzume')
