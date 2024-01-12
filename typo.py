abe0fd2762ecab7745bef13a386cb520d0a2e2b5
charmi <155721667+charmikamindaguditi@users.noreply.github.com>
2024-01-12 10:38:19 +0530

    Update typo.py

--- typo.py
+++ typo.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.header('Anime')
-st.write('Suzume')
+st.header('Chichhore')
+st.write('woh din')
 st.image('https://www.youtube.com/watch?v=xC1cj9zhh6k&pp=ygUHd29oIGRpbg%3D%3D',caption='suzume')
